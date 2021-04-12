c98d7ea5b511c094fa3eb61a661582e336ac08b7
Sanchit Kalra <sanchit.kalra1@gmail.com>
2021-04-12 17:35:48 +0530

    Updated to SO code converter.c

--- converter.c
+++ converter.c
@@ -1,39 +1,40 @@
 // Converts an input string to it's respective ASCII matrix.
 
 #include <string.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include "converter.h"
 
-// Converts the entire string into an multi-dimensional array.
-int convert(char text[]){
+struct convert {
+    int n, m;
+    int *arr;
+} convert(char text[]){
 
     // copy the input text into a local store.
-    char store[strlen(text)];
+    size_t textlen = strlen(text);
+    char store[textlen + 3 + 1];
     strcpy(store, text);
 
     // make sure the length of the input string is a multiple of 3 or make it so.
-    int excess = strlen(store)%3;
+    int excess = textlen % 3;
     char excess_spaces[3] = "   ";
     if (excess != 0) {
       strncat(store, excess_spaces, 3-excess);
     }
+    size_t storelen = strlen(store);
+
+    // allocate VLA with dynamic storage
+    int (*arr)[storelen / 3] = malloc(3 * sizeof *arr);
 
     // covert the source into an array
-    int arr[3][strlen(store)/3];
-    int steps = strlen(store)/3;
+    int steps = storelen / 3;
     for (int i = 0; i < steps; i++) {
       int t[3];
       for (int k = 0; k < 3; k++) {
         t[k] = (int) store[3*i+k];
         arr[k][i] = t[k];
       }
-      // for (int k = 0; k < 3; k++) {
-      //   printf("%c ", store[3*i+k]);
-      //   printf("%d ", t[k]);
-      //   printf("%c\n", arr[k][i]);
-      // }
     }
 
-    return arr;
-
+    return (struct convert){ .n = 3, .m = steps, .arr = (int*)arr };
 }
